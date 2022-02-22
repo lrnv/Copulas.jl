8fb05e7b44b1d94bcd6eebae93e1446130654d8c
Oskar Laverny <oskar.laverny@gmail.com>
2022-02-22 17:25:26 +0100

    reviewing imports and typo

--- src/Copulas.jl
+++ src/Copulas.jl
@@ -1,50 +1,54 @@
 module Copulas
+    import Base
+    import Random
+    import SpecialFunctions
+    import GSL
+    import Roots
+    using Distributions
+    using StatsBase
 
-import Base
-import Random
-import SpecialFunctions
-import GSL
-import Roots
-using Distributions
-using StatsBase
-# Write your package code here.
-include("utils.jl")
-include("Copula.jl")
-include("EmpiricalCopula.jl")
-include("SklarDist.jl")
+    # Standard copulas and stuff. 
+    include("utils.jl")
+    include("Copula.jl")
+    include("EmpiricalCopula.jl")
+    include("SklarDist.jl")
+    export pseudos, 
+           EmpiricalCopula, 
+           SklarDist
 
-# These three distributions might be merged in Distrbutions.jl one day. 
-include("univariate_distributions/Sibuya.jl")
-include("univariate_distributions/Logarithmic.jl")
-include("univariate_distributions/AlphaStable.jl")
+    # These three distributions might be merged in Distrbutions.jl one day. 
+    include("univariate_distributions/Sibuya.jl")
+    include("univariate_distributions/Logarithmic.jl")
+    include("univariate_distributions/AlphaStable.jl")
 
-include("EllipticalCopula.jl")
-include("EllipticalCopulas/GaussianCopula.jl")
-include("EllipticalCopulas/TCopula.jl")
+    # Elliptical copulas
+    include("EllipticalCopula.jl")
+    include("EllipticalCopulas/GaussianCopula.jl")
+    include("EllipticalCopulas/TCopula.jl")
+    export GaussianCopula, 
+           TCopula
 
-include("ArchimedeanCopula.jl")
-include("ArchimedeanCopulas/IndependentCopula.jl")
-include("ArchimedeanCopulas/ClaytonCopula.jl")
-include("ArchimedeanCopulas/JoeCopula.jl")
-include("ArchimedeanCopulas/GumbelCopula.jl")
-include("ArchimedeanCopulas/FrankCopula.jl")
-include("ArchimedeanCopulas/AMHCopula.jl")
+    # Archimedean copulas
+    include("ArchimedeanCopula.jl")
+    include("ArchimedeanCopulas/IndependentCopula.jl")
+    include("ArchimedeanCopulas/ClaytonCopula.jl")
+    include("ArchimedeanCopulas/JoeCopula.jl")
+    include("ArchimedeanCopulas/GumbelCopula.jl")
+    include("ArchimedeanCopulas/FrankCopula.jl")
+    include("ArchimedeanCopulas/AMHCopula.jl")
+    export IndependentCopula, 
+           ClaytonCopula,
+           JoeCopula,
+           GumbelCopula,
+           FrankCopula,
+           AMHCopula
 
-######### Copulas 
-export EmpiricalCopula
-export GaussianCopula
-export TCopula
-export ClaytonCopula
-export AMHCopula
-export JoeCopula
-export FrankCopula
-export GumbelCopula
-export IndependentCopula
-
-######### Distributions
-export SklarDist
-
-######### utilitiaries
-export pseudos
+    # Others. 
+    include("MiscelaneousCopulas/MCopula.jl")
+    include("MiscelaneousCopulas/WCopula.jl")
+    include("MiscelaneousCopulas/SurvivalCopula.jl")
+    export MCopula,
+        Wcopula,
+        SurvivalCopula
 
 end
