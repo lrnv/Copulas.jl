8ed6e4e991a3f1bdb1cbc7b7d1fcc9b2c662586b
Oskar Laverny <oskar.laverny@gmail.com>
2022-08-16 18:05:51 +0200

    adding a test_throws

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,7 +51,7 @@ end
     
 
     pdf(D, ([1.0, 1.0])) # this is fine
-    @test_throws cdf(D, ([1.0, 1.0])) # this produces error due to non-existance of cdf of multivariate gaussian in Distributions.jl
+    @test_throws MethodError cdf(D, ([1.0, 1.0])) # this produces error due to non-existance of cdf of multivariate gaussian in Distributions.jl
     @test 1==1
 end
 
