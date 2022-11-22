6117c81499becae42f88ea143654d1974382204e
Oskar Laverny <oskar.laverny@gmail.com>
2022-11-22 11:08:35 +0100

    adding some tests from BivariateCopulas.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,6 +35,33 @@ using Random
 
     # Same thing with other models ? 
 
+    Copula_Zoo = (
+        GumbelCopula(2,1.2),
+        ClaytonCopula(4,7.0),
+        GaussianCopula([1 0.3; 0.3 1]),
+        FrankCopula(5,6.0),
+        AMHCopula(3,0.7)
+    )
+
+    @testset "testing pdf/cdf values at corners" begin
+        for C in Copula_Zoo
+            d = length(C)
+            @test pdf(C,ones(d)) ≈ 0
+            @test pdf(C,zeros(d)) ≈ 0
+            @test cdf(C,ones(d)) ≈ 1
+            @test cdf(C,zeros(d)) ≈ 0    
+        end
+
+        for C in (TCopula(4,[1 0.5; 0.5 1]),)
+            d = length(C)
+            @test pdf(C,ones(d)) ≈ 0
+            @test pdf(C,zeros(d)) ≈ 0
+            @test_broken cdf(C,ones(d)) ≈ 1
+            @test_broken cdf(C,zeros(d)) ≈ 0    
+        end
+    end
+
+
     @testset "pdf/cdf archimedean" begin
         x = Normal(0,1); y = Normal(0,2);
         C = GumbelCopula(2, 1.2)  # a type of Archimedean copula
@@ -79,7 +106,7 @@ using Random
 
     @testset "working measure" begin 
         
-        for C in (ClaytonCopula(4,7.0),GumbelCopula(2, 1.2))
+        for C in Copula_Zoo
             d = length(C)
             u = zeros(d)
             v = ones(d)
@@ -108,6 +135,87 @@ using Random
         end
     end
 
+    # Check that \phi and \phi_inv are indeed inverses of each others. 
+
+    ##########" Tests took from BivairateCopulas.jl
+
+
+    @testset "Clayton" begin
+        
+        n = 10^6
+        θ = [-0.5, 2, 10]
+
+        @testset "constructor" begin
+            @test_broken isa(ClaytonCopula(2,0), Independence)
+            @test_throws ClaytonCopula(2,-2)
+
+            @test_logs (:warn, "Clayton returns a W copula for ϑ < -0.5") ClaytonCopula(2,-0.7)
+            @test_logs (:warn, "Clayton returns an independence copula for ϑ == 0.0") ClaytonCopula(2,
+                0.0
+            )
+            @test_logs (:warn, "Clayton returns an M copula for ϑ == Inf") ClaytonCopula(2,Inf)
+        end
+
+        @testset "generators" begin
+            u = [0:0.1:1;]
+            for ϑ in θ
+                c = ClaytonCopula(2,ϑ)
+                @test Copulas.φ⁻¹.(Copulas.φ.(u, c), c) ≈ u
+            end
+        end
+
+        @testset "τ" begin
+            @test τ(ClaytonCopula(2,θ[1])) == -1 / 3
+            @test τ(ClaytonCopula(2,θ[2])) == 0.5
+            @test τ(ClaytonCopula(2,θ[3])) == 10 / 12
+        end
+
+        @testset "sample" begin
+            for ϑ in θ
+                c = ClaytonCopula(2,ϑ)
+                u = rand(c, n)
+                @test corkendall(u) ≈ [1.0 τ(c); τ(c) 1.0] atol = 0.01
+            end
+        end
+
+        # @testset "rosenblatt" begin
+        #     for ϑ in θ
+        #         c = Clayton(ϑ)
+        #         u = rand(c, n)
+        #         @test corkendall(rosenblatt(u, c)) ≈ [1.0 0.0; 0.0 1.0] atol = 0.01
+        #     end
+        # end
+
+        # @testset "inverse_rosenblatt" begin
+        #     for ϑ in θ
+        #         c = Clayton(ϑ)
+        #         u = BivariateCopulas.sample(c, n)
+
+        #         @test inverse_rosenblatt(rosenblatt(u, c), c) ≈ u
+        #     end
+        # end
+
+        @testset "cdf" begin
+            x = [0:0.25:1;]
+            y = x
+
+            @test cdf.(ClaytonCopula(2,2), x, y) ≈
+                [0.0, 0.1796053020267749, 0.37796447300922725, 0.6255432421712244, 1.0]
+            @test cdf.(ClaytonCopula(2,-0.5), x, y) ≈
+                [1.0, 0.0, 0.17157287525381, 0.5358983848622453, 1.0]
+        end
+
+        @testset "density" begin
+            x = [0:0.25:1;]
+            y = x
+
+            @test isnan(pdf(ClaytonCopula(2,2), x[1], y[1]))
+            @test pdf.(ClaytonCopula(2,2), x[2:end], y[2:end]) ≈
+                [2.2965556205046926, 1.481003649342278, 1.614508582188617, 3.0]
+
+            @test pdf.(ClaytonCopula(2,-0.5), x, y) ≈ [Inf, 2.0, 1.0, 2 / 3, 0.5]
+        end
+    end
 
 end
 
