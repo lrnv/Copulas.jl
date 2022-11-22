b0e27411ff08da45962d74be9ae8ed8d90c6ff11
Oskar Laverny <oskar.laverny@gmail.com>
2022-11-22 10:29:17 +0100

    Fix a typo in the algorithm

--- src/Copula.jl
+++ src/Copula.jl
@@ -29,17 +29,17 @@ function measure(C::CT, u,v) where {CT<:Copula}
     # We use a gray code according to the proposal at https://discourse.julialang.org/t/looping-through-binary-numbers/90597/6
 
     eval_pt = copy(u)
-    d = length(C)
+    d = length(C) # is known at compile time 
     r = zero(eltype(u))
     graycode = 0    # use a gray code to flip one element at a time
     which = fill(false, d) # false/true to use u/v for each component (so false here)
-    r += Copulas.cdf(C,eval_pt)
+    r += Copulas.cdf(C,eval_pt) # the sign is always 0. 
     for s = 1:(1<<d)-1
         graycode′ = s ⊻ (s >> 1)
         graycomp = trailing_zeros(graycode ⊻ graycode′) + 1
         graycode = graycode′
         eval_pt[graycomp] = (which[graycomp] = !which[graycomp]) ? v[graycomp] : u[graycomp]
-        r += (-1)^s * Copulas.cdf(C,eval_pt)
+        r += (-1)^(s+d) * Copulas.cdf(C,eval_pt)
     end
     return r
 end
