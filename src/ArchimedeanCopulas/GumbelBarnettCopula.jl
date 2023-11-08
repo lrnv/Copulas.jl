69f796558daaf525270fd4c4280463a6fdbadb87
Santiago Jimenez Ramos <santymax9807@gmail.com>
2023-11-08 10:33:14 -0300

    Changes to be committed: 	new file:   src/ArchimedeanCopulas/GumbelBarnettCopula.jl 	new file:   src/ArchimedeanCopulas/InvGaussianCopula.jl 	modified:   src/MiscellaneousCopulas/PlackettCopula.jl Changes not staged for commit: 	modified:   src/Copulas.jl

---
+++ src/ArchimedeanCopulas/GumbelBarnettCopula.jl
@@ -0,0 +1,49 @@
+"""
+GumbelBarnettCopula{d,T}
+
+Fields:
+  - θ::Real - parameter
+
+Constructor
+
+    GumbelBarnettCopula(d, θ)
+
+```math
+\\phi(t) = \\exp{θ^{-1}(1-e^{t})}, 0 < \\theta \\leq 1.
+```
+
+More details about Gumbel-Barnett copula are found in Joe, H. (2014). Dependence modeling with copulas. CRC press, Page.437
+"""
+
+struct GumbelBarnettCopula{d,T} <: ArchimedeanCopula{d}
+    θ::T
+end
+GumbelBarnettCopula(d, θ) = 0 < θ <= 1 ? GumbelBarnettCopula{d, typeof(θ)}(θ) : @error "Theta must be in the range (0,1]."
+ϕ(  C::GumbelBarnettCopula,       t) = exp(-(C.θ)^(-1)*(1-exp(t)))
+ϕ⁻¹(C::GumbelBarnettCopula,       t) = log(1-C.θ*log(t))
+
+function τ(C::GumbelBarnettCopula)
+    # Define the function to integrate
+    f(x) = -x * (1 - C.θ * log(x)) * log(1 - C.θ * log(x)) / C.θ
+    
+    # Calculate the integral using GSL
+    result, _ = gsl_integration_qags(f, 0.0, 1.0, [C.θ], 1e-7, 1000)
+    
+    return 1+4*result
+end
+function τ⁻¹(::Type{GumbelBarnettCopula}, τ)
+    if τ == zero(τ)
+        return τ
+    end
+    
+    # Define an anonymous function that takes a value x and computes τ 
+    #for a GumbelBarnettCopula with θ = x
+    τ_func(x) = τ(GumbelBarnettCopula{d, Float64}(x))
+    
+    # Use the bisection method to find the root
+    x = Roots.find_zero(x -> τ_func(x) - τ, (0.0, 1.0))    
+    return x
+end
+
+import WilliamsonTransforms
+williamson_dist(C::GumbelBarnettCopula{d,T}) where {d,T} = WilliamsonTransforms.𝒲₋₁(t -> ϕ(C.θ,t),d)
