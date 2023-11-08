25c08b0aa493c35aab26984a591a40bfa653002b
Oskar Laverny <oskar.laverny@gmail.com>
2023-11-08 18:03:30 +0100

    Add a coherence test and a bugfix (#55)

--- src/univariate_distributions/WilliamsonFromFrailty.jl
+++ src/univariate_distributions/WilliamsonFromFrailty.jl
@@ -6,6 +6,13 @@ struct WilliamsonFromFrailty{TF,d} <: Distributions.ContinuousUnivariateDistribu
 end
 function Distributions.rand(rng::Distributions.AbstractRNG, D::WilliamsonFromFrailty{TF,d}) where {TF,d}
     f = rand(rng,D.frailty_dist)
-    sy = sum(.-log.(rand(rng,d)))
+    sy = rand(rng,Distributions.Erlang(d))
     return sy/f
+end
+function Distributions.cdf(D::WilliamsonFromFrailty{TF,d}, x::Real) where {TF,d}
+    # how to compute this cdf ? 
+    return 1 - Distributions.expectation(
+        e -> Distributions.cdf(D.frailty_dist,x/e),
+        Distributions.Erlang(d)
+    )
 end
