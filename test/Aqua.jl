4ddfa02e4e8668b99ab5df34a5d95227aea4b98f
Oskar Laverny <oskar.laverny@gmail.com>
2023-11-09 15:06:29 +0100

    typo

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -1,14 +1,7 @@
 @testitem "Aqua.jl" begin
     using Aqua
     Aqua.test_all(
-      YourPackage;
+      Copulas;
       ambiguities=false,
-      unbound_args=true,
-      undefined_exports=true,
-      project_extras=true,
-      stale_deps=(ignore=[:SomePackage],),
-      deps_compat=(ignore=[:SomeOtherPackage],),
-      project_toml_formatting=true,
-      piracy=false,
     )
 end
