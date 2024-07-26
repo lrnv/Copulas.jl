7dccc0f8cb1371462744774e61a5245bee68d6c1
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-07-26 02:16:50 +0200

    retype the A

--- src/ExtremeValueCopulas/MOCopula.jl
+++ src/ExtremeValueCopulas/MOCopula.jl
@@ -41,12 +41,9 @@ struct MOCopula{P} <: ExtremeValueCopula{P}
     end
 end
 
-function 𝜜(C::MOCopula, t::Real)
+function 𝘈(C::MOCopula, t::Real)
     λ1, λ2, λ12 = C.λ1, C.λ2, C.λ12
-    
-    A = max(t + (1-t) * (λ2)/(λ2 + λ12), (t-1) + t * (λ1)/(λ1 + λ12))
-    
-    return A
+    return max(t + (1-t) * (λ2)/(λ2 + λ12), (t-1) + t * (λ1)/(λ1 + λ12))
 end
 
 function Distributions._rand!(rng::Distributions.AbstractRNG, C::MOCopula, u::AbstractVector{T}) where {T<:Real}
