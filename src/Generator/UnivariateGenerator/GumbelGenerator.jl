1ef9131eb5313cac5a37a7b9353b9ba876484254
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-09-18 16:56:57 +0200

    implement bivariate logdensity for gumbel to remove overflows. Fixes #215

--- src/Generator/UnivariateGenerator/GumbelGenerator.jl
+++ src/Generator/UnivariateGenerator/GumbelGenerator.jl
@@ -39,7 +39,12 @@ end
 max_monotony(G::GumbelGenerator) = Inf
 ϕ(  G::GumbelGenerator, t) = exp(-t^(1/G.θ))
 ϕ⁻¹(G::GumbelGenerator, t) = (-log(t))^G.θ
-# ϕ⁽¹⁾(G::GumbelGenerator, t) =  First derivative of ϕ
+function ϕ⁽¹⁾(G::GumbelGenerator, t)
+    # first derivative of ϕ
+    a = 1/G.θ
+    tam1 = t^(a-1)
+    return - a * tam1 * exp(-tam1*t)
+end
 # ϕ⁽ᵏ⁾(G::GumbelGenerator, k, t) = kth derivative of ϕ
 τ(G::GumbelGenerator) = ifelse(isfinite(G.θ), (G.θ-1)/G.θ, 1)
 function τ⁻¹(::Type{T},τ) where T<:GumbelGenerator 
@@ -55,3 +60,20 @@ function τ⁻¹(::Type{T},τ) where T<:GumbelGenerator
     end
 end
 williamson_dist(G::GumbelGenerator, d) = WilliamsonFromFrailty(AlphaStable(α = 1/G.θ, β = 1,scale = cos(π/(2G.θ))^G.θ, location = (G.θ == 1 ? 1 : 0)), d)
+
+
+function Distributions._logpdf(C::ArchimedeanCopula{2,G}, u) where {G<:GumbelGenerator}
+    !all(0 .<= u .<= 1) && return eltype(u)(-Inf) # if not in range return -Inf
+
+    θ = C.G.θ
+    x₁, x₂ = -log(u[1]), -log(u[2])
+    lx₁, lx₂ = log(x₁), log(x₂)
+
+    y = (x₁ + x₂) + (θ-1) * (lx₁ + lx₂)
+    y == Inf && return Inf # shortcut
+
+    a = LogExpFunctions.logaddexp(θ * lx₁, θ * lx₂)
+    b = a/θ
+    c = a - log(θ-1)
+    return y + b + LogExpFunctions.log1pexp(c) - a - c - exp(b) 
+end
