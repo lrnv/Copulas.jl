5dce23a28862970896190b71a8fc54f56d4e40e8
Dilum Aluthge <dilum@aluthge.com>
2024-11-13 18:37:16 -0500

    Change the `reltol` kwarg to `rtol`

--- src/Copula.jl
+++ src/Copula.jl
@@ -25,13 +25,13 @@ end
 function _cdf(C::CT,u) where {CT<:Copula}
     f(x) = Distributions.pdf(C,x)
     z = zeros(eltype(u),length(C))
-    return HCubature.hcubature(f,z,u,reltol=sqrt(eps()))[1]
+    return HCubature.hcubature(f,z,u,rtol=sqrt(eps()))[1]
 end
 function ρ(C::Copula{d}) where d
     F(x) = Distributions.cdf(C,x)
     z = zeros(d)
     i = ones(d)
-    r = HCubature.hcubature(F,z,i,reltol=sqrt(eps()))[1]
+    r = HCubature.hcubature(F,z,i,rtol=sqrt(eps()))[1]
     return 12*r-3
 end
 function τ(C::Copula)
