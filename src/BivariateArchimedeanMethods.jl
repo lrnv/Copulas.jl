e4d8b71255018c5be346be21101c456d5d6fd190
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-01-13 12:45:30 +0100

    type fix

--- src/BivariateArchimedeanMethods.jl
+++ src/BivariateArchimedeanMethods.jl
@@ -6,7 +6,8 @@ function _cdf(C::ArchimedeanCopula{2,G}, u) where {G<:GumbelGenerator}
     return 1 - LogExpFunctions.cexpexp(LogExpFunctions.logaddexp(θ * lx₁, θ * lx₂) / θ)
 end
 function Distributions._logpdf(C::ArchimedeanCopula{2,G}, u) where {G<:GumbelGenerator}
-    !all(0 .< u .<= 1) && return eltype(u)(-Inf) # if not in range return -Inf
+    T = promote_type(Float64, eltype(u))
+    !all(0 .< u .<= 1) && return T(-Inf) # if not in range return -Inf
 
     θ = C.G.θ
     x₁, x₂ = -log(u[1]), -log(u[2])
