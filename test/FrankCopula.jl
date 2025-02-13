91fe3695a384e60f8086675716d7771236bd41cf
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-02-13 14:58:43 +0100

    Add FrankCopula rosenblatt tests

--- test/FrankCopula.jl
+++ test/FrankCopula.jl
@@ -1,6 +1,24 @@
 @testitem "Extreme frank density test" begin
     using Distributions
     F = FrankCopula(2, 60)
-    den = pdf(F,[0.70, 0.66])
+    den = pdf(F, [0.70, 0.66])
     @test true
+end
+
+@testitem "Frank Rosenblatt" begin
+    using StatsBase
+
+    dimensions = [2, 2, 5]
+    parameters = [-1.0, 5.0, 10.0]
+
+    for (d, θ) in zip(dimensions, parameters)
+        C = FrankCopula(d, θ)
+        u = rand(C, 10^5)
+        U = rosenblatt(C, u)
+        for i in 1:(d - 1)
+            for j in (i + 1):d
+                @test corkendall(U[i, :], U[j, :]) ≈ 0.0 atol = 0.01
+            end
+        end
+    end
 end
