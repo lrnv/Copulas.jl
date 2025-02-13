7f0d33b016594656e17b74d707e233aa327b235e
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-02-13 14:53:21 +0100

    Add AMHCopula tests

---
+++ test/AMHCopula.jl
@@ -0,0 +1,17 @@
+@testitem "AMH Rosenblatt" begin
+    using StatsBase
+
+    dimensions = [2, 2, 3, 5]
+    parameters = [0.5, -0.5, -0.3, 0.8]
+
+    for (d, θ) in zip(dimensions, parameters)
+        C = AMHCopula(d, θ)
+        u = rand(C, 10^5)
+        U = rosenblatt(C, u)
+        for i in 1:(d - 1)
+            for j in (i + 1):d
+                @test corkendall(U[i, :], U[j, :]) ≈ 0.0 atol = 0.01
+            end
+        end
+    end
+end
