9f47d198bfcba30976c67cd917d29ccdd5f698c7
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-07-25 16:21:29 +0200

    Bring back Clayton tests

--- test/ClaytonCopula.jl
+++ test/ClaytonCopula.jl
@@ -28,3 +28,54 @@
     @test isa(ClaytonCopula(2,-1), WCopula)
     @test isa(ClaytonCopula(2,Inf), MCopula)
 end
+
+@testitem "Clayton derivatives" begin
+    using ForwardDiff
+
+    for G in [Copulas.ClaytonGenerator(2.0), Copulas.ClaytonGenerator(-0.5)]
+        @test ForwardDiff.derivative(x -> Copulas.ϕ(G, x), 10.0) ≈ Copulas.ϕ⁽¹⁾(G, 10.0)
+        @test ForwardDiff.derivative(x -> Copulas.ϕ(G, x), 10.0) ≈ Copulas.ϕ⁽ᵏ⁾(G, Val(1), 10.0)
+        @test ForwardDiff.derivative(x -> Copulas.ϕ⁽¹⁾(G, x), 10.0) ≈ Copulas.ϕ⁽ᵏ⁾(G, Val(2), 10.0)
+
+        @test ForwardDiff.derivative(x -> Copulas.ϕ⁻¹(G, x), 0.5) ≈ Copulas.ϕ⁻¹⁽¹⁾(G, 0.5)
+    end
+end
+
+@testitem "Clayton rand" begin
+    using StatsBase
+
+    dimensions = [2, 2, 3, 5]
+    parameters = [2.5, -0.5, -0.3, 2.0]
+
+    for (d, θ) in zip(dimensions, parameters)
+        C = ClaytonCopula(d, θ)
+        u = rand(C, 10^5)
+        τ = Copulas.τ(C)
+        for i in 1:(d - 1)
+            for j in (i + 1):d
+                @test corkendall(u[i, :], u[j, :]) ≈ τ rtol = 0.1
+            end
+        end
+    end
+end
+
+@testitem "Clayton Rosenblatt" begin
+    using StatsBase
+
+    dimensions = [2, 2, 3, 5]
+    parameters = [2.5, -0.5, -0.3, 2.0]
+
+    for (d, θ) in zip(dimensions, parameters)
+        C = ClaytonCopula(d, θ)
+        u = rand(C, 10^5)
+        U = rosenblatt(C, u)
+        for i in 1:(d - 1)
+            for j in (i + 1):d
+                @test corkendall(U[i, :], U[j, :]) ≈ 0.0 atol = 0.01
+            end
+        end
+        u = rand(C, 10^4)
+        U = rosenblatt(C, u)
+        @test u ≈ inverse_rosenblatt(C, U)
+    end
+end
