fc009102824559fc68a26c7ecd00baa25781c790
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-09-05 11:57:45 +0200

    Add details to frailty distributons

--- src/UnivariateDistribution/Sibuya.jl
+++ src/UnivariateDistribution/Sibuya.jl
@@ -8,6 +8,8 @@ struct Sibuya{T<:Real} <: Distributions.DiscreteUnivariateDistribution
     end
     Sibuya{T}(p) where T = Sibuya(T(p))
 end
+Base.minimum(::Sibuya) = 0
+Base.maximum(::Sibuya) = Inf
 function Distributions.rand(rng::Distributions.AbstractRNG, d::Sibuya{T}) where {T <: Real}
     u = rand(rng, T)
     if u <= d.p
@@ -23,4 +25,12 @@ function Distributions.rand(rng::Distributions.AbstractRNG, d::Sibuya{T}) where 
         return ceil(Ginv)
     end
     return fGinv
+end
+Distributions.mgf(D::Sibuya, t) = 1-(-expm1(t))^(D.p)
+function Distributions.cdf(d::Sibuya, u::Real)
+    k = trunc(u)
+    return 1 - abs(binom(d.p-1, k))
+end
+function Distributions.logpdf(d::Sibuya, x::Real)
+    insupport(d, x) ? log(abs(binom(d.p, k))) : -Inf
 end
