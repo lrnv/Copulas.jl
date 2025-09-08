180889b3f96ec5ab4b40fe7824d0c961ed190c45
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-09-08 10:50:22 +0200

    Finally fix the BivEDDistortion

--- src/UnivariateDistribution/Distortions/BivEVDistortion.jl
+++ src/UnivariateDistribution/Distortions/BivEVDistortion.jl
@@ -6,17 +6,20 @@ struct BivEVDistortion{TC,T} <: Distortion
     j::Int8
     uⱼ::T
 end
-function Distributions.logcdf(D::BivEVDistortion, u::Real)
-    x, y = -log(u), -log(D.uⱼ)
-    s = x + y
-    w = x / s
-    Aw, dAw = A(D.C, w), dA(D.C, w)
-
+function Distributions.logcdf(D::BivEVDistortion, z::Real)
     if D.j == 2
-        # condition sur la deuxième composante (V = uⱼ)
+        # Condition on the second variable : V = D.uⱼ, free = u=z
+        x, y = -log(z), -log(D.uⱼ)
+        s = x + y
+        w = x / s
+        Aw, dAw = A(D.C, w), dA(D.C, w)
         return -s * Aw + y + log(Aw - w * dAw)
     else
-        # condition sur la première composante (U = uⱼ)
+        # Condition on the first variable : U = D.uⱼ, free = v=z
+        x, y = -log(D.uⱼ), -log(z)
+        s = x + y
+        w = x / s
+        Aw, dAw = A(D.C, w), dA(D.C, w)
         return -s * Aw + x + log(Aw + (1 - w) * dAw)
     end
 end
