9a80b6bf8ce23febb828ef7977cab4dbe9e76506
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-09-18 16:41:34 +0200

    [Tests] update Aqua.jl test structure and clarify persistent task handling

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -1,7 +1,8 @@
 @testitem "Aqua.jl" begin
-    using Aqua
-    Aqua.test_all(
-      Copulas;
-      ambiguities=false,
-    )
+  using Aqua
+  Aqua.test_all(
+    Copulas;
+    persistent_tasks = VERSION != v"1.10.10", # Disable persistent tasks only on Julia 1.10.10 (workaround for that release)
+    ambiguities = false,
+  )
 end
