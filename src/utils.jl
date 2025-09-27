8120deebaf94410ae514cc2bf7860b6ab2468b83
santymax98 <santymax9807@gmail.com>
2025-09-26 23:50:54 -0500

    add corentropy

--- src/utils.jl
+++ src/utils.jl
@@ -276,7 +276,9 @@ function entropy(U::AbstractMatrix; k::Int=5, p::Real=Inf, leafsize::Int=32)
         logcd = d*log(2*SpecialFunctions.gamma(1 + 1/p)) - SpecialFunctions.loggamma(1 + d/p)
         H += logcd + (d / n) * sum(log.(ρ))
     end
-    return H
+    t = clamp(2H, -700.0, 0.0)
+    r = sqrt(max(0.0, 1 - exp(t)))
+    return (H = H, I = -H, r = r)
 end
 
 function corblomqvist(X::AbstractMatrix{<:Real})
@@ -350,4 +352,63 @@ function corgini(X::AbstractMatrix{<:Real})
         end
     end
     return C
+end
+function corentropy(X::AbstractMatrix{<:Real}; k::Int=5, p::Real=Inf, leafsize::Int=32, signed::Bool=false)
+    # We expect the number of dimension to be the second axes here, 
+    # contrary to the whole package but to be coherent with 
+    # StatsBase.corspearman and StatsBase.corkendall. 
+    m, n = size(X)
+    Cnan = Vector{Bool}(undef, n)
+    for j in 1:n
+        Cnan[j] = any(isnan, @view X[:, j])
+    end
+    Ucol = [Cnan[j] ? Float64[] : collect(@view X[:, j]) for j in 1:n]
+    H  = zeros(Float64, n, n); I  = zeros(Float64, n, n); R  = Matrix{Float64}(LinearAlgebra.I, n, n)
+    R[LinearAlgebra.diagind(R)] .= 1.0
+    Rsg = nothing
+    Tτ  = nothing
+    if signed
+        Rsg = Matrix{Float64}(LinearAlgebra.I, n, n); Rsg[LinearAlgebra.diagind(Rsg)] .= 1.0
+        Tτ  = Matrix{Float64}(LinearAlgebra.I, n, n)
+    end
+    Ub = Array{Float64}(undef, 2, m)
+    @inbounds for j in 2:n
+        if Cnan[j]
+            H[:, j] .= NaN; H[j, :] .= NaN; H[j, j] = 0.0
+            I[:, j] .= NaN; I[j, :] .= NaN; I[j, j] = 0.0
+            R[:, j] .= NaN; R[j, :] .= NaN; R[j, j] = 1.0
+            if signed
+                Rsg[:, j] .= NaN; Rsg[j, :] .= NaN; Rsg[j, j] = 1.0
+                Tτ[:, j]  .= NaN; Tτ[j, :]  .= NaN; Tτ[j, j]  = 1.0
+            end
+            continue
+        end
+        uj = Ucol[j]
+        for i in 1:j-1
+            if Cnan[i]
+                H[i, j] = H[j, i] = NaN
+                I[i, j] = I[j, i] = NaN
+                R[i, j] = R[j, i] = NaN
+                if signed
+                    Rsg[i, j] = Rsg[j, i] = NaN
+                    Tτ[i, j]  = Tτ[j, i]  = NaN
+                end
+                continue
+            end
+            ui = Ucol[i]
+            Ub[1, :] .= ui; Ub[2, :] .= uj
+            est = entropy(Ub; k=k, p=p, leafsize=leafsize)
+            H[i, j] = H[j, i] = est.H
+            I[i, j] = I[j, i] = est.I
+            R[i, j] = R[j, i] = est.r
+
+            if signed
+                τij = StatsBase.corkendall(hcat(ui, uj))
+                Tτ[i, j]  = Tτ[j, i]  = τij[1, 2]
+                Rsg[i, j] = Rsg[j, i] = sign(τij[1, 2]) * est.r
+            end
+        end
+    end
+
+    return signed ? (; H, I, r_signed=Rsg) : (; H, I, r=R)
 end
