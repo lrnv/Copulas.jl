b768792fb07ad88e535d5699b5a565385a4dec7e
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-09-30 12:21:47 +0200

    restore all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,17 +4,4 @@ using TestItemRunner
 # @run_package_tests filter=ti->(:GumbelBarnettCopula in ti.tags || :ArchimedeanCopula in ti.tags || :FrankCopula in ti.tags)
 # you can add verbose=true here 
 
-@run_package_tests filter=ti->(
-    :FGMCopula in ti.tags ||
-    :PlackettCopula in ti.tags ||
-    :SurvivalCopula in ti.tags ||
-    :GaussianCopula in ti.tags ||
-    :TCopula in ti.tags ||
-    :GalambosCopula in ti.tags ||
-    :AsymMixedCopula in ti.tags ||
-    :CuadrasAugeCopula in ti.tags ||
-    :HuslerReisCopula in ti.tags ||
-    :MixedCopula in ti.tags ||
-    :EmpiricalEVCopula in ti.tags ||
-    :ArchimaxCopula in ti.tags
-)
+@run_package_tests
