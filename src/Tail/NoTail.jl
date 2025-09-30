83a3ecf045476ede1e8fef2dda610e408af513f0
santymax98 <santymax9807@gmail.com>
2025-09-30 12:20:31 +0200

    ExtremeValue ok

--- src/Tail/NoTail.jl
+++ src/Tail/NoTail.jl
@@ -6,3 +6,6 @@ Corresponds to the case where the pickads function is identically One, which mea
 struct NoTail <: Tail end
 A(::NoTail, t::NTuple{d, <:Real}) where d = one(eltype(t))
 ExtremeValueCopula(d, ::NoTail) = IndependentCopula(d)
+A(::NoTail, t::Real) = 1.0
+dA(::NoTail, ::Real) = 0.0
+d²A(::NoTail, ::Real) = 0.0
