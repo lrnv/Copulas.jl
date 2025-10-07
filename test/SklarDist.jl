0ae8c98ad22be8468a2d0ef832f8f50a06de9186
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-10-07 21:32:04 +0200

    remove flay test

--- test/SklarDist.jl
+++ test/SklarDist.jl
@@ -9,7 +9,6 @@
     u = rand(rng,MyD,1000)
     rand!(rng, MyD,u)
     fit(SklarDist{ClaytonCopula,Tuple{LogNormal,Pareto,Beta}},u)
-    fit(SklarDist{GaussianCopula,Tuple{LogNormal,Pareto,Beta}},u)
     @test 1==1
     # loglikelyhood(MyD,u)
 end
