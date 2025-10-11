755f01cd644c5587e3b703af172b3181b49327f7
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-10-11 04:13:39 +0200

    retore full runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,21 +7,21 @@ const rng = StableRNG(123)
 
 # You can comment the lines to avoid running some tests while you develop:
 testfiles = [
-    # "Aqua",
-    # "ArchimedeanCopulas",
-    # "ConditionalDistribution",
-    # "EllipticalCopulas",
-    # "FittingTest",
-    # "MiscelaneousCopulas",
-    # "SklarDist",
+    "Aqua",
+    "ArchimedeanCopulas",
+    "ConditionalDistribution",
+    "EllipticalCopulas",
+    "FittingTest",
+    "MiscelaneousCopulas",
+    "SklarDist",
     "GenericTests",
 ]
 
 # You can override the definition of this GenericTestFilter if you want. 
-# GenericTestFilter(C) = true # the default value lets every copula go through. 
+GenericTestFilter(C) = true # the default value lets every copula go through. 
 
 # An example: 
-GenericTestFilter(C) = C isa ArchimedeanCopula && C.G isa WilliamsonGenerator # You can filter on your model. 
+# GenericTestFilter(C) = C isa JoeCopula || C isa GumbelCopula # You can filter on your model. 
 
 @testset verbose=true "Copulas.jl testings"  begin
     @testset verbose=true "f = $f.jl" for f in testfiles  
