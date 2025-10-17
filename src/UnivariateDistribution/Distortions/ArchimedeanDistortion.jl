c974872b3a21da4e2061348b481a6cd0bdf30821
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-10-17 16:02:12 +0200

    [Feat] Archimedean conditionals: remove superfluous type parameters (#339)

--- src/UnivariateDistribution/Distortions/ArchimedeanDistortion.jl
+++ src/UnivariateDistribution/Distortions/ArchimedeanDistortion.jl
@@ -1,17 +1,18 @@
 ###########################################################################
 #####  ArchimedeanCopula fast-paths
 ###########################################################################
-struct ArchimedeanDistortion{TG, T, p} <: Distortion
+struct ArchimedeanDistortion{TG, T} <: Distortion
     G::TG
+    p::Int
     sJ::T
     den::T
-    ArchimedeanDistortion(G::TG, p::Int, sJ::T, den::T) where {T<:Real, TG} = new{TG, T, p}(G, sJ, den)
+    ArchimedeanDistortion(G::TG, p::Int, sJ::T, den::T) where {T<:Real, TG} = new{TG, T}(G, p, sJ, den)
 end
-function Distributions.cdf(D::ArchimedeanDistortion{TG, T, p}, u::Real) where {TG, T, p}
-    return ϕ⁽ᵏ⁾(D.G, p, D.sJ + ϕ⁻¹(D.G, float(u))) / D.den
+function Distributions.cdf(D::ArchimedeanDistortion{TG, T}, u::Real) where {TG, T}
+    return ϕ⁽ᵏ⁾(D.G, D.p, D.sJ + ϕ⁻¹(D.G, float(u))) / D.den
 end
-function Distributions.quantile(D::ArchimedeanDistortion{TG, T, p}, α::Real) where {TG, T, p}
-    y = ϕ⁽ᵏ⁾⁻¹(D.G, p, α * D.den; start_at = D.sJ)
+function Distributions.quantile(D::ArchimedeanDistortion{TG, T}, α::Real) where {TG, T}
+    y = ϕ⁽ᵏ⁾⁻¹(D.G, D.p, α * D.den; start_at = D.sJ)
     return ϕ(D.G, y - D.sJ)
 end
 ## ConditionalCopula moved next to ArchimedeanCopula definition
