54221446ce18349e8b46cb1a2315eeb350bd8bc5
Oskar Laverny <oskar.laverny@univ-amu.fr>
2026-07-20 14:52:31 +0200

    Enforce support consistently for elementary distortions

--- src/UnivariateDistribution/Distortions/MDistortion.jl
+++ src/UnivariateDistribution/Distortions/MDistortion.jl
@@ -5,7 +5,12 @@ struct MDistortion{T} <: Distortion
     v::T
     j::Int8
 end
-Distributions.cdf(D::MDistortion, u::Real) = min(u / D.v, 1)
-Distributions.pdf(D::MDistortion, u::Real) = u > D.v ? zero(u) : one(u) / D.v
+function Distributions.cdf(D::MDistortion, u::Real)
+    z = u / D.v
+    return clamp(z, zero(z), one(z))
+end
 Distributions.quantile(D::MDistortion, α::Real) = α * D.v
-Distributions.logpdf(D::MDistortion, u::Real) = (0 <= u <= D.v) ? -log(D.v) : -Inf
+function Distributions.logpdf(D::MDistortion, u::Real)
+    T = promote_type(typeof(float(u)), typeof(D.v))
+    return 0 <= u <= D.v ? T(-log(D.v)) : T(-Inf)
+end
--- src/UnivariateDistribution/Distortions/NoDistortion.jl
+++ src/UnivariateDistribution/Distortions/NoDistortion.jl
@@ -2,9 +2,11 @@
 #####  Identity distortion (used e.g. by IndependentCopula)
 ###########################################################################
 struct NoDistortion <: Distortion end
-Distributions.cdf(::NoDistortion, u::Real) = u
-Distributions.pdf(::NoDistortion, u::Real) = one(u)
+Distributions.cdf(::NoDistortion, u::Real) = clamp(u, zero(u), one(u))
 Distributions.quantile(::NoDistortion, α::Real) = α
 (::NoDistortion)(X::Distributions.UnivariateDistribution) = X
 (::NoDistortion)(::Distributions.Uniform) = Distributions.Uniform()
-Distributions.logpdf(::NoDistortion, u::Real) = zero(u)
+function Distributions.logpdf(::NoDistortion, u::Real)
+    T = typeof(float(u))
+    return 0 <= u <= 1 ? zero(T) : T(-Inf)
+end
