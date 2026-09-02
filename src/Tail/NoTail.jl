811180553d6f2cea720a079e2dd06fc7b052daab
Oskar Laverny <oskar.laverny@univ-amu.fr>
2026-09-02 10:53:36 +0200

    Add random number generation for ExtremeValueCopula and update TCopula test cases with numerical tolerance

--- src/Tail/NoTail.jl
+++ src/Tail/NoTail.jl
@@ -10,3 +10,8 @@ A(::NoTail, t::Real) = 1.0
 ℓ(::NoTail, x) = sum(x)
 dA(::NoTail, ::Real) = 0.0
 d²A(::NoTail, ::Real) = 0.0
+
+function Distributions._rand!(rng::Distributions.AbstractRNG, C::ExtremeValueCopula{d,<:NoTail}, X::AbstractMatrix{T},) where {d,T<:Real}
+    Random.rand!(rng, X)
+    return X
+end
