ef2e9aed1b0a64849187c0460ad2362c3a6dcc65
Documenter.jl <documenter@juliadocs.github.io>
2026-09-12 09:12:12 +0000

    build based on 64763fc

--- previews/PR463/assets/manual_intro.md.CEs9lKyM.js
+++ previews/PR463/assets/manual_intro.md.CEs9lKyM.js
@@ -91,7 +91,7 @@ import{_ as n,o as e,c as i,am as l,j as t,a as s}from"./chunks/framework.B6LYjl
 <span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">BIC:                   2587.219</span></span>
 <span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">Converged:             true</span></span>
 <span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">Iterations:            24</span></span>
-<span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">Elapsed:               0.036s</span></span>
+<span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">Elapsed:               0.035s</span></span>
 <span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">────────────────────────────────────────────────────────────────────────────────</span></span>
 <span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">[ Dependence metrics ]</span></span>
 <span class="line"><span style="--shiki-light:#24292e;--shiki-dark:#e1e4e8;">────────────────────────────────────────────────────────────────────────────────</span></span>
