ca336896ec8d61509ef9a700d437ecbc4578d3c5
Oskar Laverny <oskar.laverny@univ-amu.fr>
2026-09-16 13:55:21 +0200

    up

--- src/Copula.jl
+++ src/Copula.jl
@@ -140,8 +140,7 @@ Distributions.logcdf(C::Copula, v::AbstractVector) = log(Distributions.cdf(C,v))
     @boundscheck length(u) == d || throw(DimensionMismatch(
         "input dimension does not match copula dimension",
     ))
-    any(x -> x < zero(x), u) && return zero(u[1])
-    any(x -> x > one(x), u)  && return zero(u[1])
+    all(x -> 0 <= x <= 1, u) || return eltype(u)(-Inf)
     value = Distributions._logpdf(C, u)
     isnan(value) || return value
     return _resolve_boundary_logpdf(value, u)
