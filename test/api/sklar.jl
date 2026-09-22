e767de28f8e5ebe0ba7937e36499adc12251c967
Oskar Laverny <oskar.laverny@univ-amu.fr>
2026-09-22 10:56:47 +0200

    Remove direct Paramorph assertions from Sklar API tests

--- test/api/sklar.jl
+++ test/api/sklar.jl
@@ -15,8 +15,6 @@
     @test_throws DimensionMismatch SklarDist(C, (Normal(),))
     @test params(D) isa Tuple
     @test StatsBase.dof(D) == 4
-    @test StatsBase.dof(D) ==
-          Copulas.Paramorph.intrinsic_dimension(D)
     @test 0 <= cdf(D, x) <= 1
     @test logcdf(D, x) ≈ log(cdf(D, x))
     @test pdf(D, x) >= 0
@@ -56,8 +54,7 @@
     @test StatsBase.dof(clayton_joint) == 4
 
     D3 = SklarDist(GaussianCopula{3}(0.3), (Normal(), Exponential(), Gamma(2, 1)))
-    @test StatsBase.dof(D3) ==
-          Copulas.Paramorph.intrinsic_dimension(D3)
+    @test StatsBase.dof(D3) == 9
     x3 = [0.1, 1.2, 0.8]
     joint = condition(D3, 1, x3[1])
     @test length(joint) == 2
